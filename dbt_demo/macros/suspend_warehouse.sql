8576bdf637a3d43a35f2e786f43d94923abf88d5
NaderSalameh <74309258+NaderSalameh@users.noreply.github.com>
2023-04-16 11:55:50 -0400

    Update suspend_warehouse.sql

--- dbt_demo/macros/suspend_warehouse.sql
+++ dbt_demo/macros/suspend_warehouse.sql
@@ -1,7 +1,7 @@
 {% macro suspend(warehouse_name) %}
 
     {% set sql %}
-        alter warehouse {{warehouse_name}} resume
+        alter warehouse {{warehouse_name}} suspend
     {% endset %}
 
     {% set table = run_query(sql)  %}
